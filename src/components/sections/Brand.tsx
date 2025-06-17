0f4525d49ab13795b6e01423c8dbe8a3ffac44df
imflawlezz <egorart6@gmail.com>
2025-06-17 15:51:18 +0200

    chore: fix tabulation

--- src/components/sections/Brand.tsx
+++ src/components/sections/Brand.tsx
@@ -5,10 +5,10 @@ export const Brand = () => {
         <div className="overflow-x-hidden">
             <section className="relative my-16 sm:my-18 md:my-24 lg:my-32 px-4 sm:px-10 md:px-20 lg:px-30 max-w-screen-xl mx-auto min-h-[600px] flex items-center">
                 <div className="w-full lg:w-1/2 z-10">
-          <span className="text-3xl sm:text-4xl md:text-5xl font-bold uppercase leading-tight text-black">
-            Improve <span className="font-light">your brand </span>
-            with an awesome site.
-          </span>
+                    <span className="text-3xl sm:text-4xl md:text-5xl font-bold uppercase leading-tight text-black">
+                    Improve <span className="font-light">your brand </span>
+                    with an awesome site.
+                    </span>
 
                     <div className="mt-12 space-y-10">
                         <div>
